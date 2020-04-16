e9b1bea92c3c54823357df4aee0e6e0337306cc2
prakash-reddy-ctrl <62732525+prakash-reddy-ctrl@users.noreply.github.com>
2020-04-16 20:44:53 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,29 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import torch
+from torch.jit import script, trace
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import csv
+import random
+import re
+import os
+import unicodedata
+import codecs
+from io import open
+import itertools
+import math
+
+
+USE_CUDA = torch.cuda.is_available()
+device = torch.device("cuda" if USE_CUDA else "cpu")
+
+corpus_name = "chitchat"
+corpus = os.path.join("chatbot", corpus_name)
 datafile = os.path.join(corpus, "cornell_movie_lines.txt")
 
 
@@ -107,7 +133,7 @@ def loadPrepareData(corpus, corpus_name, datafile, save_dir):
 
 
 # Load/Assemble voc and pairs
-save_dir = os.path.join("data", "save")
+save_dir = os.path.join("chatbot", "save")
 voc, pairs = loadPrepareData(corpus, corpus_name, datafile, save_dir)
 # Print some pairs to validate
 print("\npairs:")
